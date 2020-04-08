28e35941b5c246651e39078485173b1028317450
mcanovese <marco.canovese@studenti.unipd.it>
2020-04-08 16:09:04 +0200

    trattasi di funzionante PARE

--- QtCessionaria/GUI/usermenu.cpp
+++ QtCessionaria/GUI/usermenu.cpp
@@ -1,6 +1,25 @@
 #include "usermenu.h"
 
-Usermenu::Usermenu()
+Usermenu::Usermenu(QWidget* p):parent(p),
+    menu(new QMenu("File",this)),
+    salva(new QAction("Salva",this)),
+    carica(new QAction("Carica",this)),
+    esci(new QAction("Esci",this)),
+
+    ricerca(new QAction("Ricerca",this)),
+    inserisci(new QAction("Inserisci",this))
+
 {
+
+    QList<QAction*> actionList;
+    actionList.push_back(salva);
+    actionList.push_back(carica);
+    actionList.push_back(esci);
+
+    menu->addActions(actionList);
+
+    addMenu(menu);
+    addAction(ricerca);
+    addAction(inserisci);
 
 }
--- QtCessionaria/GUI/usermenu.h
+++ QtCessionaria/GUI/usermenu.h
@@ -2,10 +2,34 @@
 #define USERMENU_H
 
 
-class Usermenu
-{
+#include <QObject>
+#include <qmenubar.h>
+#include <qwidget.h>
+
+
+class Usermenu : public QMenuBar {
+    Q_OBJECT
+private:
+    QWidget* parent;
+    QMenu* menu;
+
+    QAction* salva;
+    QAction* carica;
+    QAction* ricerca;
+    QAction* inserisci;
+    QAction* esci;
+
 public:
-    Usermenu();
+
+    Usermenu(QWidget* = nullptr);
+    QMenu *getMenu() const;
+    QAction *getSalva() const;
+    QAction *getCarica() const;
+    QAction *getRicerca() const;
+    QAction *getInserici() const;
+    QAction *getEsci() const;
+
 };
+
 
 #endif // USERMENU_H
