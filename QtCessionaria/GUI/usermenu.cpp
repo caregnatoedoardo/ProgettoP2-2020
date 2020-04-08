efcac3d864198769a82c0e9c8156565c9e53c396
mcanovese <marco.canovese@studenti.unipd.it>
2020-04-08 16:00:36 +0200

    usermenu.cpp usermenu.h

---
+++ QtCessionaria/GUI/usermenu.cpp
@@ -0,0 +1,6 @@
+#include "usermenu.h"
+
+Usermenu::Usermenu()
+{
+
+}
---
+++ QtCessionaria/GUI/usermenu.h
@@ -0,0 +1,11 @@
+#ifndef USERMENU_H
+#define USERMENU_H
+
+
+class Usermenu
+{
+public:
+    Usermenu();
+};
+
+#endif // USERMENU_H
